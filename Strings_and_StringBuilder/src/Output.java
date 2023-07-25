8e5764edd931c88fcc16efb14059b3aa87b388c2
MATI UL REHMAN <iammatiz00@gmail.com>
2023-07-25 23:14:45 +0500

    Strings continues

---
+++ Strings_and_StringBuilder/src/Output.java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+
+public class Output {
+    public static void main(String[] args) {
+        System.out.println("Ali Hassan");
+        float a = 12.34567f;
+        System.out.printf("PI: %.4f\n",Math.PI);
+        System.out.printf("Formatted Number upto 2 decimal: %.2f\n",a);
+        System.out.println(Arrays.toString(new int[]{1, 2, 3, 4}));
+    }
+}
