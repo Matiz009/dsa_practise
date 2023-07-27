26fa3518ea9b7e471d10dd454d47b7e46eebc2da
MATI UL REHMAN <iammatiz00@gmail.com>
2023-07-27 18:12:43 +0500

    patterns questions added

---
+++ Patterns/src/Main.java
@@ -0,0 +1,60 @@
+public class Main {
+    public static void main(String[] args) {
+       pattern6(6);
+
+    }
+    static void pattern1(int n){
+        for (int row = 1; row <= n; row++) {
+            for (int col = 1; col <= row; col++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+    static void pattern2(int n){
+        for (int row = 1; row <= n; row++) {
+            for (int col = 1; col <= n; col++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+    static void pattern3(int n){
+        for (int row = n; row >= 0; row--) {
+            for (int col = row; col >= 0; col--) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+    static void pattern4(int n){
+        for (int row = 1; row <= n; row++) {
+            for (int col = 1; col <= row; col++) {
+                System.out.print(col+" ");
+            }
+            System.out.println();
+        }
+    }
+    static void pattern5(int n){
+        for (int row = 0; row < 2* n; row++) {
+            int totalColsInRow = row > n ? 2 * n - row : row;
+            for (int col = 0; col < totalColsInRow; col++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+    static void pattern6(int n){
+        for (int row = 0; row < 2* n; row++) {
+            int totalColsInRow = row > n ? 2 * n - row : row;
+            int spaces = n-totalColsInRow;
+            for (int s = 0; s < spaces; s++) {
+                System.out.println(" ");
+            }
+            for (int col = 0; col < totalColsInRow; col++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
