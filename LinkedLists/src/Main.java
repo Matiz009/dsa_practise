7b5e698086ae62288c5e14305b3733f56f8bc3f9
MATI UL REHMAN <iammatiz00@gmail.com>
2023-08-01 17:16:00 +0500

    LinkedLists crd

--- LinkedLists/src/Main.java
+++ LinkedLists/src/Main.java
@@ -1,12 +1,23 @@
 public class Main {
     public static void main(String[] args) {
         Single_LinkedList singleLinkedList = new Single_LinkedList();
-        singleLinkedList.insertAtFirst(23);
-        singleLinkedList.insertAtFirst(21);
-        singleLinkedList.insertAtFirst(56);
-        singleLinkedList.insertAtFirst(34);
-        singleLinkedList.insertAtEnd(43);
-        singleLinkedList.insertAtEnd(93);
+        singleLinkedList.insertAtFirst(45);
+        singleLinkedList.insertAtFirst(55);
+        singleLinkedList.insertAtFirst(5);
+        singleLinkedList.insertAtFirst(4);
+        singleLinkedList.insertAtEnd(41);
+        singleLinkedList.insertAtEnd(81);
+        singleLinkedList.insertAtIndex(34,5);
+        singleLinkedList.display();
+        System.out.println(singleLinkedList.getSize());
+        System.out.println(singleLinkedList.deleteFirst());
+        singleLinkedList.display();
+        System.out.println(singleLinkedList.getSize());
+        System.out.println(singleLinkedList.deleteEnd());
+        System.out.println(singleLinkedList.deleteAtIndex(2));
+        singleLinkedList.display();
+        singleLinkedList.insertAtEnd(56);
+        System.out.println(singleLinkedList.getSize());
         singleLinkedList.display();
     }
 }
--- LinkedLists/src/Single_LinkedList.java
+++ LinkedLists/src/Single_LinkedList.java
@@ -2,6 +2,12 @@ public class Single_LinkedList {
     private Node head;
     private Node tail;
     private int size;
+
+    public int getSize() {
+        System.out.print("size: \t");
+        return size;
+    }
+
     public Single_LinkedList(){
         this.size=0;
     }
@@ -35,7 +41,25 @@ public class Single_LinkedList {
        Node node=new Node(value);
        tail.next=node;
        tail=node;
+       tail.next=null;
        size++;
+    }
+    public void insertAtIndex(int value,int index){
+        if(index==0){
+            insertAtFirst(value);
+            return;
+        }
+       if(index==size){
+           insertAtEnd(value);
+           return;
+       }
+       Node head2 = head;
+        for (int i = 1; i < index; i++) {
+            head2=head2.next;
+        }
+        Node node = new Node(value,head2.next);
+        head2.next=node;
+        size++;
     }
     public void display(){
         Node temp = head;
@@ -44,5 +68,46 @@ public class Single_LinkedList {
             temp = temp.next;
         }
         System.out.println("END.");
+    }
+    public int deleteFirst(){
+        int value=head.value;
+        head=head.next;
+        if(head==null){
+            tail=null;
+        }
+        size--;
+        return value;
+    }
+    public int deleteEnd(){
+        if(size<=1){
+            return deleteFirst();
+        }
+        Node secondLast = get(size-2);
+        int value= tail.value;
+        tail=secondLast;
+        tail.next=null;
+        size--;
+        return value;
+    }
+    public int deleteAtIndex(int index){
+        if(index==0){
+            return deleteFirst();
+        }
+        if (index==size){
+            return deleteEnd();
+        }
+        Node behindNum=get(index-1);
+        Node aboveNum=get(index+1);
+        Node num=get(index);
+        behindNum.next=aboveNum;
+        size--;
+        return num.value;
+    }
+    public Node get(int index){
+        Node node =head;
+        for (int i = 0; i < index; i++) {
+            node=node.next;
+        }
+        return node;
     }
 }
