057dba7fa15d81fddcd630bd98e98ed90c02d920
MATI UL REHMAN <iammatiz00@gmail.com>
2023-08-05 01:52:13 +0500

    Binary Trees completed

--- Binary_Trees/src/Main.java
+++ Binary_Trees/src/Main.java
@@ -5,6 +5,6 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         BinaryTree tree = new BinaryTree();
         tree.populate(scanner);
-        tree.display();
+        tree.prettyDisplay();
     }
 }
